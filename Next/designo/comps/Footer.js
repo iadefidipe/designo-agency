f5906881c4c1739acff97b2fbba0d4e5ff45f1ac
Israel <israeladefidipe@gmail.com>
2021-10-17 03:35:22 +0100

    �  fix: footer styling in styled component done

--- Next/designo/comps/Footer.js
+++ Next/designo/comps/Footer.js
@@ -2,51 +2,48 @@ import React from 'react'
 import Logo from './Logo'
 import { menuData } from '../data/menuData'
 import Nav from './Nav'
-import styles from '../styles/modules/Footer.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
+import { FooterContent, FooterHeader, StyledFooter, FooterAddress, FooterContact, FooterSocial, FooterContainer } from '../styles/component-styles/footer.style'
 
 const Footer = () => {
     return (
-        <footer className={styles.footer} >
+        
+        <StyledFooter  >
 
-            <div className='page-container' >
 
-                <div className={styles.footerHeader} >
-                    <Logo  scroll={true} href={menuData.homeLink} src={ menuData.footer.logo} alt={menuData.footer.alt}  />
+                <FooterContainer>
+                    <FooterHeader   >
+                        <Logo  scroll={true} href={menuData.homeLink} src={ menuData.footer.logo} alt={menuData.footer.alt}  />
+                        <Nav />
+                    </FooterHeader>
+                    
+                    <FooterContent>
+                        <FooterAddress >
+                            <p>{menuData.address.office}</p>
+                            <p>{menuData.address.street}</p>
+                            <p>{menuData.address.city}</p>
+                    
+                        </FooterAddress>
+                        <FooterContact  >
+                            <p>{menuData.contact.title}</p>
+                            <p> P: {menuData.contact.phone}</p>
+                            <p> M: {menuData.contact.email}</p>
+                        </FooterContact>
+                        <FooterSocial  >
+                            {menuData.social.map( (social,index) => (
+                                <Link href={social.link} key={index} ><a>
+                                    <Image src={social.icon} alt={social.label} />
+                                </a></Link>
+                            ) )}
+                        </FooterSocial >
+                    </FooterContent>
+                </FooterContainer>
 
-                    <Nav />
-                </div>
-
-                <div className={styles.footerContent} >
-
-                    <div className={styles.footerAddress} >
-                        <p>{menuData.address.office}</p>
-                        <p>{menuData.address.street}</p>
-                        <p>{menuData.address.city}</p>
-                        
-                    </div>
-
-                    <div className={styles.footerContact} >
-                        <p>{menuData.contact.title}</p>
-                        <p> P: {menuData.contact.phone}</p>
-                        <p> M: {menuData.contact.email}</p>
-                    </div>
-
-                    <div className={styles.footerSocial} >
-                        {menuData.social.map( (social,index) => (
-                            <Link href={social.link} key={index} ><a>
-                                <Image src={social.icon} alt={social.label} />
-                            </a></Link>
-                        ) )}
-                    </div>
-
-                </div>
-
-            </div>
             
             
-        </footer>
+            
+        </StyledFooter>
     )
 }
 
