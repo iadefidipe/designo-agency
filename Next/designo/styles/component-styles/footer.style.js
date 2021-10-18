d03885b335ea1bb7a3e91c6d4616d71776680c98
Israel <israeladefidipe@gmail.com>
2021-10-18 20:48:54 +0100

    ✔ fix: fixed footer styling

--- Next/designo/styles/component-styles/footer.style.js
+++ Next/designo/styles/component-styles/footer.style.js
@@ -1,103 +1,89 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
-    background: ${({theme}) => theme.colors.pryBlack};
-    padding: 144px 0 50px;
-`
+  background: ${({ theme }) => theme.colors.pryBlack};
+  padding: 144px 0 50px;
+`;
 export const FooterContainer = styled.div`
-    max-width: ${ ({theme}) => theme.maxWidth };
-    margin: 0 auto;
-    padding: ${ ({theme}) => theme.genPad } ;
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: ${({ theme }) => theme.genPad};
+
+  & nav {
+    color: ${({ theme }) => theme.colors.pryWhite};
+
+    &:hover {
+      border-bottom: none;
+    }
+  }
+
+  & > * {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  @media (max-width: 768px) {
+    text-align: center;
+    padding: 166px 0 50px;
+
+    & > *,
+    & nav {
+      flex-direction: column;
+    }
 
     & nav {
-        color: ${({theme}) => theme.colors.pryWhite };
-
-        &:hover{
-            border-bottom: none;
-        }
+      padding-top: 31px;
     }
+  }
 
-    &>* {
-        display: flex;
-        justify-content: space-between;
-    }
-
-    @media (max-width: 768px) {
-        text-align: center;
-        padding: 166px 0 50px;
-       
-
-        &>*, & nav{
-            flex-direction: column;
-        }
-
-        & nav{
-            padding-top:  31px;
-        }
-
-       
-    }
-
-    @media (max-width: 375px) {
-        padding: 235px 0 50px;
-    }
-
-`
+  @media (max-width: 375px) {
+    padding: 235px 0 50px;
+  }
+`;
 
 export const FooterHeader = styled.div`
-    align-items: center;
-  border-bottom: ${({theme}) => `1px solid ${theme.colors.secDarkGrey}`};
+  align-items: center;
+  border-bottom: ${({ theme }) => `1px solid ${theme.colors.secDarkGrey}`};
   padding: 30px 0;
 
-    @media (max-width: 768px) {
-        border-bottom: none;
+  @media (max-width: 768px) {
+    border-bottom: none;
 
-        &> div {
-            width: 100%;
-            padding-bottom: 20px;
-            border-bottom:  ${({theme}) => `1px solid ${theme.colors.secDarkGrey}`};
-        }
+    & > div {
+      width: 100%;
+      padding-bottom: 20px;
+      border-bottom: ${({ theme }) => `1px solid ${theme.colors.secDarkGrey}`};
     }
-`
+  }
+`;
 
 export const FooterContent = styled.div`
-    color: ${({theme}) => theme.colors.secLightGrey };
-    margin-top: 31px ;
+  color: ${({ theme }) => theme.colors.secLightGrey};
+  margin-top: 31px;
 
-    @media (max-width: 768px) {
-        gap: 40px;       
-    }
-`
+  @media (max-width: 768px) {
+    gap: 40px;
+  }
+`;
 
 export const FooterAddress = styled.div`
-    & p:first-child{
-        font-weight: ${({theme}) => theme.weight.mid};
-    }
-`
+  & p:first-child {
+    font-weight: ${({ theme }) => theme.weight.mid};
+  }
+`;
 
 export const FooterContact = styled.div`
-
-    &  p{
-        font-weight: ${({theme}) => theme.weight.mid}
-    }
-`
+  & p {
+    font-weight: ${({ theme }) => theme.weight.mid};
+  }
+`;
 
 export const FooterSocial = styled.div`
-    align-self: flex-end;
-    display: flex;
-    gap: 16px;
+  align-self: flex-end;
+  display: flex;
+  gap: 16px;
 
-    @media (max-width: 768px) {
-        align-self: center;      
-    }
-
-
-`
-
-
-
-
-
-
-
-
+  @media (max-width: 768px) {
+    align-self: center;
+  }
+`;
