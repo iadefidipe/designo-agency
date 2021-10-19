aadd3314e33e0164e03bded14c7b7f66aaf981f9
Israel <israeladefidipe@gmail.com>
2021-10-19 02:14:18 +0100

    ⭕  feat: Location list component crated

---
+++ Next/designo/data/LocationData.js
@@ -0,0 +1,24 @@
+import CanadaImage from "../public/assets/shared/desktop/illustration-canada.svg";
+import AustraliaImage from "../public/assets/shared/desktop/illustration-australia.svg";
+import UkImage from "../public/assets/shared/desktop/illustration-united-kingdom.svg";
+
+export const locationListData = [
+  {
+    country: "Canada",
+    image: CanadaImage,
+    label: "see our Canadian contact information",
+    anchorLink: "/locations#canada",
+  },
+  {
+    country: "Australia",
+    image: AustraliaImage,
+    label: "see our Australian contact information",
+    anchorLink: "/locations#australia",
+  },
+  {
+    country: "United Kingdom",
+    image: UkImage,
+    label: "see our United Kingdom contact information",
+    anchorLink: "/locations#united-kingdom",
+  },
+];
