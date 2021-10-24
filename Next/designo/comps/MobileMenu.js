ef85e0d6fa6b5ca0953dad637a1422686af6755c
Israel <israeladefidipe@gmail.com>
2021-10-24 01:56:13 +0100

    ✔ fix: mobile dropdown disappearss when option is clicjdke

--- Next/designo/comps/MobileMenu.js
+++ Next/designo/comps/MobileMenu.js
@@ -1,20 +1,15 @@
-import React from 'react'
-import Nav from './Nav'
-import {StyledMenu} from '../styles/component-styles/MobileMenu.style'
+import React from "react"
+import Nav from "./Nav"
+import { StyledMenu } from "../styles/component-styles/MobileMenu.style"
 
-
-const MobileMenu = () => {
-    return (
-        
-        <StyledMenu >
-            
-            <div>
-    
-                <Nav />
-                
-            </div>      
-        </StyledMenu>
-    )
+const MobileMenu = ({ toggle, setToggle }) => {
+  return (
+    <StyledMenu>
+      <div>
+        <Nav toggle={toggle} setToggle={setToggle} />
+      </div>
+    </StyledMenu>
+  )
 }
 
 export default MobileMenu
--- Next/designo/comps/Nav.js
+++ Next/designo/comps/Nav.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import Link from 'next/link'
-import { menuData } from '../data/menuData'
-import { StyledNav } from '../styles/component-styles/Nav.style'
+import React from "react"
+import Link from "next/link"
+import { menuData } from "../data/menuData"
+import { StyledNav } from "../styles/component-styles/Nav.style"
 
-const Nav = () => {
-    return (
-        
-        <StyledNav >
-            {menuData.menu.map( (menu, index) => 
-                <Link href={menu.link} key={index}><a>{menu.title}</a></Link>
-             )}
-        </StyledNav >
-    )
+const Nav = ({ toggle, setToggle }) => {
+  return (
+    <StyledNav>
+      {menuData.menu.map((menu, index) => (
+        <Link href={menu.link} key={index}>
+          <a onClick={() => setToggle(!toggle)}>{menu.title}</a>
+        </Link>
+      ))}
+    </StyledNav>
+  )
 }
 
 export default Nav
